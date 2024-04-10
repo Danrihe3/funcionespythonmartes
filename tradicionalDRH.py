70da8fabca95e6abc86b3d3e9d923fad71e5b1d1
desarrollo <desarrollo@academicos.cesde.edu.co>
2024-04-09 19:12:32 -0500

    nt8 feat Se crea ejemplo de funcion tradicional

---
+++ tradicionalDRH.py
@@ -0,0 +1,15 @@
+'''numeroUno=5
+numeroDos=10
+sumatoria=numeroUno+numeroDos
+print(sumatoria)'''
+
+
+def sumarNumeros(numeroUno,numeroDos):
+    suma=numeroUno+numeroDos
+    return  suma
+
+numeroUno=5
+numeroDos=20
+numeroTres=50
+resultado=sumarNumeros(numeroUno,numeroDos)
+print(f'El resultado es {resultado}')
